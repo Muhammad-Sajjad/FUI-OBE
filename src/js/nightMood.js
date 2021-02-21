6b813e94a29cbf6e5215b21c88c8c45b315a6ed6
Muhammad-Sajjad <sajjad.rizvi@apimatic.io>
2021-02-22 00:43:55 +0500

    UI

---
+++ src/js/nightMood.js
@@ -0,0 +1,7 @@
+function nightMood() {
+  var element = document.body;
+  element.classList.toggle("night-mood");
+
+  var logo = document.getElementById("dark");
+  logo.classList.toggle("logo-dark");
+}
